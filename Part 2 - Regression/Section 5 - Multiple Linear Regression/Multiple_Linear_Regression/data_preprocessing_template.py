2f9dcbdd905e01f329df498c55ec13601fb062de
shahaashay21 <shah.aashay21@gmail.com>
2020-01-09 21:50:21 -0800

    Multiple linear regression without backward elimination

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/data_preprocessing_template.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 23:01:22 2020
+
+@author: aashays
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Split the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train).astype(float)
+X_test = sc_X.transform(X_test).astype(float)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train.reshape(-1, 1).astype(float))"""
--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -1,45 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan  6 20:58:43 2020
+Created on Wed Jan  8 21:21:15 2020
 
 @author: aashays
 """
 
-# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
-
-# Taking care of missing data
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 # Encoding the Independent variable
-ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder = 'passthrough')
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [3])], remainder = 'passthrough')
 X = np.array(ct.fit_transform(X), dtype = np.float)
-# Encoding the Dependent variable
-y = LabelEncoder().fit_transform(y)
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
 
 # Split the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train).astype(float)
 X_test = sc_X.transform(X_test).astype(float)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train.reshape(-1, 1).astype(float))
+y_train = sc_y.fit_transform(y_train.reshape(-1, 1).astype(float))"""
 
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
+# Predicting the Test set results
+y_pred = regressor.predict(X_test);
